689518971b32c1c48729f4fef1640b7e5b9d782d
chuanze <beyond198825@163.com>
2018-07-01 14:03:09 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # SpringAOP
+
+## Spring AOP 概念
+### . 走进面向切面编程
+1. 是一种编程范式，不是编程语言
+2. 解决特定问题，不能解决所有问题
+3. 是OOP的补充，不是替代
+### AOP的初衷
+DRY：Don't Repeat Youself
+SoC：Separation of Concerns
+- 水平分离：展现层->服务层->持久层
+- 垂直分离：模块划分（订单、库存等）
+
+### Spring
+## AOP使用
+## 
---
+++ src/main/java/com/spring/aop/springaop/SpringaopApplication.java
@@ -0,0 +1,12 @@
+package com.spring.aop.springaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringaopApplication.class, args);
+    }
+}
